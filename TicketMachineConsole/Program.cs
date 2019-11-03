43c3f3f5ec3d0fec047a400f363f40cfe8880c39
Karlsson <karlsson0214@gmail.com>
2019-11-03 18:11:20 +0100

    Add project files.

---
+++ TicketMachineConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicketMachineConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try buy ticket for 1 hour 30 minutes");
+            Console.WriteLine("Current date and time: " + DateTime.Now);
+            ParkingMachine machine = new ParkingMachine(20);
+            machine.InsertMoney(30);
+            string ticketText = machine.BuyTicket();
+            Console.WriteLine("Printing ticket: ");
+            Console.WriteLine();
+            Console.WriteLine(ticketText);
+            
+        }
+    }
+}
